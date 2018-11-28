56b6fcafe1d0efef01ed0dc3c60b205ec24c354b
Colombe <c.schh@outlook.com>
2018-11-28 17:43:38 +0100

    Updates

---
+++ precache-manifest.45d4e68a065fa421be75011320f2acf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nutflux/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "848c47bd373e1998a56e",
+    "url": "/nutflux/static/js/runtime~main.848c47bd.js"
+  },
+  {
+    "revision": "05523c7dbb89a68b4156",
+    "url": "/nutflux/static/js/main.05523c7d.chunk.js"
+  },
+  {
+    "revision": "4c393b2e2adabba373cd",
+    "url": "/nutflux/static/js/1.4c393b2e.chunk.js"
+  },
+  {
+    "revision": "05523c7dbb89a68b4156",
+    "url": "/nutflux/static/css/main.ec9911e9.chunk.css"
+  },
+  {
+    "revision": "d9f63dcc99431057676a06e5898ee7fa",
+    "url": "/nutflux/index.html"
+  }
+];
